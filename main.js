f910b12deb6477e879be7cf2890733ff50141f21
Jony-Mia <ajonymia321@gmail.com>
2026-02-27 17:24:15 +0600

    project completed

---
+++ main.js
@@ -0,0 +1,22 @@
+const text = document.getElementById('text')
+const btn = document.getElementById('btn')
+const list = document.getElementById('list');
+btn.onclick=()=>{
+  if(!text.value==''){
+      const li = document.createElement('li');
+    const deleteBtn = document.createElement('button');
+    const btn = document.querySelector('#delete');
+    
+    li.className="flex justify-between my-1 font-mono font-medium text-[20px]"
+    li.textContent=`${text.value}`
+
+    deleteBtn.innerHTML= `<button id='delete'><i class="fa-duotone text-red-400 fa-solid fa-trash"></i></button>`
+    deleteBtn.addEventListener('click',()=>li.remove())
+
+    li.appendChild(deleteBtn);    
+    list.appendChild(li)
+    text.value=''
+  }else{
+    alert('You enter empty task')
+  }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+<h1> To do list app </h1>
+<h2> You Can do these: </h2>
+
+<ul>
+    <li> Add item</li>
+    <li> Remove item</li>
+    <li> Edit item</li>
+
+</ul>
